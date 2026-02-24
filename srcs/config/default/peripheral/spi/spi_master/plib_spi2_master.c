4485fa856a34c75855c0916b2b19025a3a18eecb
David Coetzee <davec6505@gmail.com>
2026-02-24 13:35:20 +0000

    SPI2 Mode 3 (CKP=1 CKE=0) 2MHz MSSEN=0 for TMC5160

--- srcs/config/default/peripheral/spi/spi_master/plib_spi2_master.c
+++ srcs/config/default/peripheral/spi/spi_master/plib_spi2_master.c
@@ -52,13 +52,13 @@
 static volatile SPI_OBJECT spi2Obj;
 
 #define SPI2_CON_MSTEN                      (1UL << _SPI2CON_MSTEN_POSITION)
-#define SPI2_CON_CKP                        (0UL << _SPI2CON_CKP_POSITION)
-#define SPI2_CON_CKE                        (1UL << _SPI2CON_CKE_POSITION)
+#define SPI2_CON_CKP                        (1UL << _SPI2CON_CKP_POSITION)
+#define SPI2_CON_CKE                        (0UL << _SPI2CON_CKE_POSITION)
 #define SPI2_CON_MODE_32_MODE_16            (0UL << _SPI2CON_MODE16_POSITION)
 #define SPI2_CON_ENHBUF                     (1UL << _SPI2CON_ENHBUF_POSITION)
 #define SPI2_CON_MCLKSEL                    (0UL << _SPI2CON_MCLKSEL_POSITION)
-#define SPI2_CON_MSSEN                      (1UL << _SPI2CON_MSSEN_POSITION)
-#define SPI2_CON_SMP                        (0UL << _SPI2CON_SMP_POSITION)
+#define SPI2_CON_MSSEN                      (0UL << _SPI2CON_MSSEN_POSITION)
+#define SPI2_CON_SMP                        (1UL << _SPI2CON_SMP_POSITION)
 
 void SPI2_Initialize ( void )
 {
@@ -82,18 +82,18 @@ void SPI2_Initialize ( void )
     IFS4CLR = 0x10000;
 
     /* BAUD Rate register Setup */
-    SPI2BRG = 2;
+    SPI2BRG = 12;
 
     /* CLear the Overflow */
     SPI2STATCLR = _SPI2STAT_SPIROV_MASK;
 
     /*
     MSTEN = 1
-    CKP = 0
-    CKE = 1
+    CKP = 1
+    CKE = 0
     MODE<32,16> = 0
     ENHBUF = 1
-    MSSEN = 1
+    MSSEN = 0
     MCLKSEL = 0
     */
     SPI2CONSET = (SPI2_CON_MSSEN | SPI2_CON_MCLKSEL | SPI2_CON_ENHBUF | SPI2_CON_MODE_32_MODE_16 | SPI2_CON_CKE | SPI2_CON_CKP | SPI2_CON_MSTEN | SPI2_CON_SMP);
